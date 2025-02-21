99c1dddb721b6675ae1b7bb872ebb059ffbe186f
Vania Ilchuk <vaniailchuk@MacBook-Pro-3.local>
2025-02-21 11:54:39 -0500

    #2 added player triangle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import * 
+from player import Player
+
 
 def main():
     
@@ -8,6 +10,10 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True: 
         for event in pygame.event.get():
@@ -15,7 +21,11 @@ def main():
                 return
         
         screen.fill("#000000") 
+        player.draw(screen)
         pygame.display.flip() 
+
+        result = clock.tick(60)
+        dt = result / 1000 
         
         
        
